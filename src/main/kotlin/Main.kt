9976b8596db168620e2481dd5373c5e46e697bdc
phillister-yekebedi <phillisteryekebedi@gmail.com>
2023-02-02 21:06:39 +0300

    student records

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    var name = arrayOf("yekebediTech", "bathsheba", "abigael", "methuselah")
+    println(name)
+    var age = arrayOf(22, 18, 10, 13)
+    println(age)
+    var phonenumber = arrayOf(71355728, 726855987, 798814633)
+    println(phonenumber)
+    var weight = arrayOf(60, 70, 28, 20)
+    println(weight)
+    var citizenship = arrayOf("kenyan", "tanzania", "ugandan", "kenyan")
+
+
+}
