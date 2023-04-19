b0252ab40ab85b2a994f308fbb4d3d675a193e9f
Phillister-yekebedi <otienoyekebedi@gmail.com>
2023-04-19 20:17:09 +0300

    Strings

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,3 @@
-import sun.management.Agent
 
 fun main() {
     greetMe("World")
@@ -17,6 +16,13 @@ fun main() {
     println(y)
     sentence("practice")
     sentence("Grit and persevearnce")
+    str()
+    param("Otieno", 22)
+    println(lens("Wycliffe"))
+    var s = lens("CodeHive bootcamp")
+    println(s)
+    var m = lens("HopperLab")
+    println(m)
 
 
 
@@ -69,4 +75,44 @@ fun sentence(words:String){
     var send = "$words"+" " + "makes perfect"
     println(sent)
 }
+                                                       //Strings 19/4/2023
+//Given a string “akirachix”. Write a function that prints out a string composed
+//of the first, third and fourth characters of the string
+fun str(){
+    val Str = "Akirachix"
+    println(Str[1])
+    println(Str[3])
+    println(Str[4])
+}
+//Write a function that takes in 2 parameters, name and age and returns a
+//String with this structure “Hi, my name is x and I am y years old.” Where x and
+//y are the provided name and age respectively.
+fun param(name: String,age:Int){
+    println("Hi, my name is ${name} and i am ${age} years old" )
+
+}
+//Write a function that takes in a String and returns its length
+fun lens(name1:String):Int{
+    return name1.length
+}
+
+//Write a function that takes in a name and prints out “That’s me!” when your
+////name is passed to it, otherwise it prints out “I don’t know who that is”
+fun thatMe(names1: String){
+    if(names1 == "Sage"){
+        println("That's me")
+    }
+    else{
+        println("I don't know who that is")
+    }
+
+}
+
+
+
+
+
+
+
+
 
